f03138303b97dcdbbb5689c03af919eeb43c66fd
Kapil Rathore <kapil.rathore@tokopedia.com>
2020-02-06 23:18:15 +0530

    initial commit

---
+++ SwiftUIDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by kapilrathore-mbp on 06/02/20.
+//  Copyright © 2020 Kapil Rathore. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
